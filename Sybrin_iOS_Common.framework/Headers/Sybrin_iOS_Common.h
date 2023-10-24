0b54f239578dd93f133e19f301bc0e939429af89
JenkinsCI <releasemanager@sybrin.co.za>
2023-10-24 14:43:49 +0200

    [CI] Updated Sybrin.iOS.SDK.Common.Test to version 1000.0.1.beta

---
+++ Sybrin_iOS_Common.framework/Headers/Sybrin_iOS_Common.h
@@ -0,0 +1,19 @@
+//
+//  Sybrin_iOS_Common.h
+//  Sybrin.iOS.Common
+//
+//  Created by Nico Celliers on 2020/08/06.
+//  Copyright © 2020 Sybrin Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sybrin_iOS_Common.
+FOUNDATION_EXPORT double Sybrin_iOS_CommonVersionNumber;
+
+//! Project version string for Sybrin_iOS_Common.
+FOUNDATION_EXPORT const unsigned char Sybrin_iOS_CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sybrin_iOS_Common/PublicHeader.h>
+
+
